ded9067629b8c4b52b1193ac10c01e8a0e85808a
Cassia39 <kamikami1417@gmail.com>
2026-04-29 22:45:07 +0900

    ファイルの削除
    いらないファイルを削除した。

--- web_design_class/js/class5_1.js
+++ web_design_class/js/class5_1.js
@@ -1,24 +1,34 @@
-box = new Array();
-box[0] = 12;
-box[1] = 34;
-box[2] = 56;
-box[3] = 78;
-box[4] = 90;
+let n = [];
 
-for (i = 0; i < 5; i++) {
-    sum += box[i];
+n[0] = 12;
+n[1] = 34;
+n[2] = 56;
+n[3] = 78;
+n[4] = 90;
+
+let total = 0;
+
+for (let i = 0; i < n.length; i++) {
+  total = total + n[i];
 }
 
-alert("合計："+sum);
+alert(total);
 
-gankin = 1000;
-riritsu = 0.07;
-nensu = 20;
-for (i=1; i<= nensu; i++) 
-{
-    kane = gankin * Math.pow((1+riritsu),i);
-    barH = kane/10;
-    document.write('<img src="./../image/pixel.gif" width="8" height="`+barH+`">');
+let graph = document.getElementById("graph");
 
+for (let i = 0; i < n.length; i++) {
+  let barBox = document.createElement("div");
+  barBox.className = "bar-box";
 
+  let bar = document.createElement("div");
+  bar.className = "bar";
+  bar.style.height = n[i] * 2 + "px";
+
+  let value = document.createElement("div");
+  value.className = "value";
+  value.textContent = n[i];
+
+  barBox.appendChild(bar);
+  barBox.appendChild(value);
+  graph.appendChild(barBox);
 }
